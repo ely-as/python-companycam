a11975d165c402370f772ae53f6e5c8614257a08
ely-as <mail@ely.as>
2022-12-14 16:29:38 -0800

    Test that manager paths accept queries when required

--- tests/test_companycam_v2_managers.py
+++ tests/test_companycam_v2_managers.py
@@ -12,6 +12,16 @@ CLIENT_V2 = utils.ClientTestHelper(
 OPENAPI = utils.OpenAPI()
 
 
+def OPENAPI_find_path_with_skip(method: str, url: str) -> utils.OpenAPIPath:  # type: ignore[return]
+    try:
+        return OPENAPI.find_path(method=method, url=url)
+    except ValueError:
+        pytest.skip(
+            "Path appears to be missing in OpenAPI spec, but this is covered by a "
+            "different unit test."
+        )
+
+
 @pytest.mark.parametrize("path", CLIENT_V2.manager_paths)
 def test_all_manager_paths_exist_in_OpenAPI_spec(path: utils.ManagerPath) -> None:
     assert OPENAPI.find_path(method=path.method, url=path.url)
@@ -27,14 +37,9 @@ def test_managers_have_same_number_of_paths_as_OpenAPI_spec() -> None:
 def test_all_manager_paths_return_same_type_as_in_OpenAPI_spec(  # noqa: C901 (complexity: 7)
     manager_path: utils.ManagerPath,
 ) -> None:
-    try:
-        method = manager_path.method
-        url = manager_path.url
-        openapi_path = OPENAPI.find_path(method=method, url=url)
-    except ValueError:
-        pytest.skip(
-            "Path appears to be missing, but this is covered by a different unit test."
-        )
+    method = manager_path.method
+    url = manager_path.url
+    openapi_path = OPENAPI_find_path_with_skip(method=method, url=url)
     # For HTTP 204 (No Content) responses, expect bool
     if openapi_path.status_code_2xx == "204":
         assert manager_path.return_type == bool
@@ -58,3 +63,16 @@ def test_all_manager_paths_return_successfully(
 ) -> None:
     utils.ClientSendPatcher(mocker)
     path.call(**path.filter_kwargs(**v2_model_objects.KWARGS))
+
+
+@pytest.mark.parametrize("path", CLIENT_V2.manager_paths)
+def test_all_manager_paths_accept_query_parameters_if_specified_in_OpenAPI_spec(
+    mocker: MockerFixture, path: utils.ManagerPath
+) -> None:
+    openapi_path = OPENAPI_find_path_with_skip(method=path.method, url=path.url)
+    if openapi_path.query_parameters:
+        patch = utils.ClientSendPatcher(mocker)
+        kwargs = path.filter_kwargs(**v2_model_objects.KWARGS)
+        kwargs["query"] = {"page": 2}
+        path.call(**kwargs)
+        assert "page=2" in str(patch.request.url)
